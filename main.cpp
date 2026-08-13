46ce8e7be7ba1c3553b870efdf9da7722736df4a
Zachary Rapis <zacharythanos@gmail.com>
2026-08-13 14:39:25 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
-void warriorInfo(std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter);
-void archerInfo(std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter);
-void sorcererInfo(std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter);
-void tankerInfo(std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter);
-void wardenInfo(std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter);
+struct Classes {
+    double health;
+    double stamina;
+    double damage;
+    double range;
+    std::string description;
+};
+
+void characterVerification(char& proceed);
 void checkCharacter(std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter);
+void printStats(const Classes& character);
 
 using text = std::string;
 
@@ -13,10 +18,15 @@ int main () {
 
     int role = {};
     text character;
-    text warrior;
     char proceed;
     bool hasCharacter = false;
     std::string usedCharacter;
+    
+    Classes warrior;
+    Classes archer;
+    Classes sorcerer;
+    Classes tanker;
+    Classes warden;
 
     
 
@@ -27,7 +37,7 @@ int main () {
 
     do {
     // Class selection
-    std::cout << "\nPlease choose a role.\n";
+    std::cout << "\nPlease choose a class.\n";
     std::cout << "1. Warrior\n";
     std::cout << "2. Archer\n";
     std::cout << "3. Sorcerer\n";
@@ -37,97 +47,73 @@ int main () {
 
     switch (role) {
         case 1:
-            warriorInfo(character, proceed, hasCharacter,usedCharacter);
+            character = "warrior";
+            warrior.health = 110;
+            warrior.stamina = 50;
+            warrior.damage = 10;
+            warrior.range = 5;
+            warrior.description = "The Warrior:\nA fierce mortal ready to hack and slash its way to glory.\n";
+            printStats(warrior);
+            
+            characterVerification(proceed);
             break;
         case 2:
-            archerInfo(character, proceed, hasCharacter,usedCharacter);
+            character = "archer";
+            archer.health = 110;
+            archer.stamina = 50;
+            archer.damage = 10;
+            archer.range = 5;
+            archer.description = "The Archer:\nA long range combatant able to penetrate even the thickest of defense.\n";
+            printStats(archer);
+            
+            characterVerification(proceed);
             break;
         case 3:
-            sorcererInfo(character, proceed, hasCharacter,usedCharacter);
+            character = "sorcerer";
+            sorcerer.health = 110;
+            sorcerer.stamina = 50;
+            sorcerer.damage = 10;
+            sorcerer.range = 5;
+            sorcerer.description = "The Sorcerer:\nA powerful magical being capable of controlling matter.\n";
+            printStats(sorcerer);
+            
+            characterVerification(proceed);
             break;
         case 4:
-            tankerInfo(character, proceed, hasCharacter,usedCharacter);
+            character = "tanker";
+            tanker.health = 110;
+            tanker.stamina = 50;
+            tanker.damage = 10;
+            tanker.range = 5;
+            tanker.description = "The Tanker:\nHeavy weight capable of absorbing any damage that comes in its way.\n";
+            printStats(tanker);
+            
+            characterVerification(proceed);
             break;
         case 5:
-            wardenInfo(character, proceed, hasCharacter,usedCharacter);
+            character = "warden";
+            warden.health = 110;
+            warden.stamina = 50;
+            warden.damage = 10;
+            warden.range = 5;
+            warden.description = "The Warden:\nAn ancient mystique capable of healing its allies and debuffing its enemies.\n";
+            printStats(warrior);
+            
+            characterVerification(proceed);
             break;
         default:
             std::cout << "Please enter only 1-5!\n";
             break;
     }
 
-    if (proceed == 'Y' || proceed == 'y') {
+    if (proceed == 'Y') {
     std::cout << "You are now a " << character << '\n';
+    hasCharacter = true;
     }
+    
+    
     }while(hasCharacter == false);
 
-}
-
-void warriorInfo (std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter) {
-    std::cout << "The Warrior:\nA fierce mortal ready to hack and slash its way to glory.\n";
-    std::cout << "Health: 110\n";
-    std::cout << "Stamina: 50\n";
-    std::cout << "Damage: 10\n";
-    std::cout << "Range: 5\n";
-    std::cout << "Do you want to proceed?\n";
-    std::cin >> proceed;
-
-    usedCharacter = "Warrior";
-
-    checkCharacter(character, proceed, hasCharacter, usedCharacter);
-}
-void archerInfo (std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter) {
-    std::cout << "The Archer:\nA long range combatant able to penetrate even the thickest of defense.\n";
-    std::cout << "Health: 80\n";
-    std::cout << "Stamina: 70\n";
-    std::cout << "Damage: 10\n";
-    std::cout << "Range: 8\n";
-    std::cout << "Do you want to proceed?\n";
-    std::cin >> proceed;
-
-    usedCharacter = "Archer";
-
-    checkCharacter(character, proceed, hasCharacter, usedCharacter);
-    
-}
-void sorcererInfo (std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter) {
-    std::cout << "The Sorcerer:\nA powerful magical being capable of controlling matter.\n";
-    std::cout << "Health: 80\n";
-    std::cout << "Stamina: 100\n";
-    std::cout << "Damage: 6\n";
-    std::cout << "Do you want to proceed?\n";
-    std::cin >> proceed;
-
-    usedCharacter = "Sorcerer";
-
-    checkCharacter(character, proceed, hasCharacter, usedCharacter);
-}
-void tankerInfo (std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter) {
-    std::cout << "The Tanker:\nHeavy weight capable of absorbing any damage that comes in its way.\n";
-    std::cout << "Health: 130\n";
-    std::cout << "Stamina: 50\n";
-    std::cout << "Damage: 15\n";
-    std::cout << "Do you want to proceed?\n";
-    std::cin >> proceed;
-
-    usedCharacter = "Tanker";
-
-    checkCharacter(character, proceed, hasCharacter, usedCharacter);
-    
-}
-void wardenInfo (std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter) {
-    std::cout << "The Warden:\nAn ancient mystique capable of healing its allies and debuffing its enemies.\n";
-    std::cout << "Health: 80\n";
-    std::cout << "Stamina: 100\n";
-    std::cout << "Damage: 8\n";
-    std::cout << "Heal: 10\n";
-    std::cout << "Do you want to proceed?\n";
-    std::cin >> proceed;
-
-    usedCharacter = "Warden";
-
-    checkCharacter(character, proceed, hasCharacter, usedCharacter);
-    
 }
 
 void checkCharacter (std::string& character, char& proceed, bool& hasCharacter, std::string& usedCharacter) {
@@ -138,5 +124,26 @@ void checkCharacter (std::string& character, char& proceed, bool& hasCharacter, 
         // Do nothing
     } else {
         std::cout << "Please enter only Y/N\n";
+    }
+}
+void printStats (const Classes & character) {
+    std::cout << character.description;
+    std::cout << "Health: " << character.health << '\n';
+    std::cout << "Stamina: " << character.stamina << '\n'; 
+    std::cout << "Damage: " << character.damage << '\n';
+    std::cout << "Range: " << character.range << '\n'; 
+}
+
+void characterVerification (char& proceed){
+    char proceedChoice;
+    std::cout << "Do you want to proceed?";
+    std::cin >> proceedChoice;
+    
+    if (proceedChoice == 'Y' || proceedChoice == 'y') {
+        proceed = 'Y';
+    } else if (proceed == 'N' || proceed == 'n') {
+        // Do nothing
+    } else {
+        std::cout << "Please enter a valid character (Y/N)";
     }
 }
