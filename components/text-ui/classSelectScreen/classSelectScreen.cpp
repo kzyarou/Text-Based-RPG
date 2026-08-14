eccedc07d00864186ad104c55b6de0522cadc1af
kzyarou <zacharythanos@gmail.com>
2026-08-14 23:39:31 +0000

    refactor: separate class selection screen into header and implementation

---
+++ components/text-ui/classSelectScreen/classSelectScreen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "classSelectScreen.hpp"
+
+void classSelectScreen() {
+    std::cout << "|=================================|\n"
+                 "| Choose your path.               |\n"
+                 "|                                 |\n"
+                 "| 1. Warrior                      |\n"
+                 "| 2. Huntsman                     |\n"
+                 "| 3. Sorcerer                     |\n"
+                 "| 4. Paladin                      |\n"
+                 "| 5. Priest                       |\n"
+                 "|=================================|\n\n";
+}
